4c3ec1996ed704c5668babb72e20a0e143d10211
chi.zhang <chi.zhang@shangri-la.com>
2022-12-17 21:55:38 +0800

    feat: 可播放完整MP3文件 暂时无法处理流

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::io::{BufReader, Cursor};
+use std::thread::{self, JoinHandle};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use futures::stream::StreamExt;
 use serde_json::{Value};
+use rodio::Decoder;
 
 fn print_begin() {
     println!(r#"
@@ -11,9 +14,7 @@ fn print_begin() {
 
 }
 
-#[tokio::main]
-async fn main() {
-    print_begin();
+async fn wss() {
     let url = "wss://coderadio-admin.freecodecamp.org/api/live/nowplaying/coderadio";
     let (w, _) = connect_async(url).await.unwrap();
     let (_, mut reader) = w.split();
@@ -21,7 +22,26 @@ async fn main() {
         let message = v.unwrap();
         if let Message::Text(text) = message {
             let json: Value = serde_json::from_str(text.as_str()).unwrap();
-            println!("{:?}", json);
+            println!("{:?}", json["station"]["listen_url"]);
         }
     }
+}
+fn get_audio() -> JoinHandle<()>{
+    thread::spawn(move || {
+        let url = "https://coderadio-admin.freecodecamp.org/radio/8010/radio.mp3";
+        let url = "http://websrvr90va.audiovideoweb.com/va90web25003/companions/Foundations%20of%20Rock/13.01.mp3";
+        let mut res = reqwest::blocking::get(url).unwrap();
+        // println!("{:?}", res.bytes().unwrap());
+        let mut cursor = Cursor::new(res.bytes().unwrap());
+        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+        let sink = rodio::Sink::try_new(&handle).unwrap();
+        let mut data = Decoder::new(cursor).unwrap();
+        sink.append(data);
+        sink.sleep_until_end();
+    })
+}
+#[tokio::main]
+async fn main() {
+    print_begin();
+    get_audio().join().unwrap();
 }
